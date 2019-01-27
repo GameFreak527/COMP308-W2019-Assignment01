f0fe615341de73c41f28958d147349dbb2a734fa
GameFreak527 <shivrana527@gmail.com>
2019-01-26 22:12:30 -0500

    Pages Created

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'about' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'contact' });
+});
+
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'projects' });
+});
+
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'services' });
+});
+
 module.exports = router;
